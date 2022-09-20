1a56109227d4b55e6c1f556504af2a2d44ca9dd9
Hai. Dinh Van - CMC Global DU1.3 <dvhai2@cmcglobal.vn>
2022-09-20 13:17:57 +0700

    Add Ui page Post

--- pages/posts/atomic-habit.js
+++ pages/posts/atomic-habit.js
@@ -1,12 +1,8 @@
-import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
-import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const AtomicHabit = () => (
   <Layout title="Atomic habit">
-   Don't have anything yet cause I still read that book!
+   Dont have anything yet cause I still read that book!
   </Layout>
 )
 
