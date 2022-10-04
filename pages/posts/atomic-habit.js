b54e9ab453d54bafc4551ff140b946461235fa9e
Hai. Dinh Van - CMC Global DU1.3 <dvhai2@cmcglobal.vn>
2022-10-04 13:40:51 +0700

    Feat: atomic habit

--- pages/posts/atomic-habit.js
+++ pages/posts/atomic-habit.js
@@ -1,10 +1,18 @@
+import { Text } from '@chakra-ui/react'
 import HeaderPostDetail from '../../components/header-post-detail'
 import Layout from '../../components/layouts/article'
 
 const AtomicHabit = () => (
   <Layout title="Atomic habit">
-    <HeaderPostDetail />
-    Dont have anything yet cause I still read that book!
+    <Text>
+    Do you ever feel like you just floating through life... but not actually getting closer to the person that you want to be. 
+    It usually happens around New years, you imagine all the bad habits your going to break free from, and all the good habits you will begin. 
+    "This time will be different" you say to yourself. "This time I AM going to do the things that I say I will". Only to end up back where you began shortly after and no closer to what you had envisaged(hình dung).
+    So the question is, how do you become the person you dream of becomming? How do you break free from bad habits and make the habits you desire easier and automatic?
+    Atomic Habits by James Clear answers all these questions. This book deserves a space on everyone's bookshelf! Let's jump into it.
+    </Text>
+ 
+
   </Layout>
 )
 
